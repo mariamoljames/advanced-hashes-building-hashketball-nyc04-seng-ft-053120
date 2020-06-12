94901edf5a3668ecff96a6085ea5e1a53658231d
Mariamol James <mariamol.james@gmail.com>
2020-06-12 17:55:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,11 @@
 def game_hash
   game_hash={home:{},away:{}}
+  count=0
+  while count<game_hash.length do
+    game_hash[count][:team_name]=""
+    game_hash[count][:colors]=[]
+    game_hash[count][:players]
+  end
+
 
 end
