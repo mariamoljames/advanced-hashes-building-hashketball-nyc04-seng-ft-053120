bdedb8168a038d55755b52823a6f84d7e9e9ba39
Mariamol James <mariamol.james@gmail.com>
2020-06-12 18:12:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,9 @@ def game_hash
   game_hash={home:{},away:{}}
   count=0
   while count<game_hash.length do
-    [game_hash.keys][count][:team_name]=game_hash.keys[count].to_s
-    [game_hash.keys][count][:colors]=[]
-    [game_hash.keys][count][:players]=[]
+    game_hash.keys[count][:team_name]=game_hash.keys[count].to_s
+    game_hash.keys[count][:colors]=[]
+    game_hash.keys[count][:players]=[]
   end
   binding.pry
   game_hash
